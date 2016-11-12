9f6cac7a155b10c6d32fd2ebf1120ec4b57d33e9
Anthony McDonald <anthony.mcdonald@btinternet.com>
2016-11-12 11:01:52 +0000

    Update release.sh

--- Scripts/release.sh
+++ Scripts/release.sh
@@ -1,6 +1,17 @@
-#!/bin/bash -vx
+#!/bin/bash
 # run with "bash release.sh"
-sudo cp "/home/pi/Documents/Geany Projects/Rockbus V2/main" /home/rockbus/rockbus
+
+#turn on echo
+set -x
+
+#copy rockbus to the rockbus user directory
+sudo cp "~/Documents/Rockbus/main" /home/rockbus/rockbus
+
+#set the file ownership to rockbus
 sudo chown rockbus:rockbus /home/rockbus/rockbus
-sudo cp "/home/pi/Documents/Geany Projects/Zen/main" /home/rockbus/zen
+
+#copy zen to the rockbus user directory
+sudo cp "~/Documents/Geany Projects/Zen/main" /home/rockbus/zen
+
+#set the file ownership to rockbus
 sudo chown rockbus:rockbus /home/rockbus/zen
