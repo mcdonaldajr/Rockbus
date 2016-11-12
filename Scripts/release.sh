21874c9c41cb8859c83e7d8b296dba9ae107d343
Anthony McDonald <anthony.mcdonald@btinternet.com>
2016-11-12 11:01:52 +0000

    Update release.sh

--- Scripts/release.sh
+++ Scripts/release.sh
@@ -1,6 +1,22 @@
-#!/bin/bash -vx
+#!/bin/bash
 # run with "bash release.sh"
-sudo cp "/home/pi/Documents/Geany Projects/Rockbus V2/main" /home/rockbus/rockbus
+
+#turn on echo
+set -x
+#stop the zen service or the destination files will be locked
+sudo service zen stop
+
+#copy rockbus to the rockbus user directory
+sudo cp ~/Documents/Rockbus/Rockbus/main /home/rockbus/rockbus
+
+#set the file ownership to rockbus
 sudo chown rockbus:rockbus /home/rockbus/rockbus
-sudo cp "/home/pi/Documents/Geany Projects/Zen/main" /home/rockbus/zen
+
+#copy zen to the rockbus user directory
+sudo cp ~/Documents/Rockbus/Zen/main /home/rockbus/zen
+
+#set the file ownership to rockbus
 sudo chown rockbus:rockbus /home/rockbus/zen
+
+#start the zen service with the new release
+sudo service zen start
